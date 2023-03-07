22ba5a421425b45bcaf8b540824b0f325782cf20
micoliser <micoliser@gmail.com>
2023-03-07 16:25:37 +0100

    [update] Modified console.py and file_storage.py to work with newly created classes

--- console.py
+++ console.py
@@ -9,6 +9,9 @@ from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,7 +22,10 @@ class HBNBCommand(cmd.Cmd):
         "BaseModel": BaseModel,
         "User": User,
         "State": State,
-        "City": City
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
     }
 
     def emptyline(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,12 +38,18 @@ class FileStorage:
         from models.user import User
         from models.state import State
         from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
         model_dict = {
             "BaseModel": BaseModel,
             "User": User,
             "State": State,
-            "City": City
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
         }
 
         try:
